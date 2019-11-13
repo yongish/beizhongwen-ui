689ab8fcf62b60bf78bcadcf4eff77dc30b58c39
Zhi Yong Tan <yongish@gmail.com>
2019-11-13 13:12:31 -0800

    Added store and reducer. Have not started configuing redux and props.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router' // react-router v4/v5
+import rootReducer from './reducers/index';
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './configureStore'
+
+const store = configureStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+  ,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

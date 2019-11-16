0579812995ca383d9d5d2cb95d93cd4e58339e14
Zhi Yong Tan <yongish@gmail.com>
2019-11-16 10:47:50 -0800

    Header components.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,32 @@
 import store from '../index';
 
+const API_ROOT = 'http://localhost:8080/';
+
+// todo: refresh token if fail. in middleware
+export const getScore = (uid, token) => dispatch => {
+  const fullUrl = API_ROOT + 'score/' + uid;
+  dispatch({ type: SCORE_REQUEST });
+  return fetch(fullUrl, {
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  }).then(
+    response => response.json().then(
+      json => {
+        dispatch({ type: SCORE_SUCCESS, response: json });
+      }
+    ),
+    error => {
+      dispatch({ type: SCORE_FAILURE, error: error });
+    }
+  )
+}
+
 export const selectTab = tab => dispatch => {
   dispatch({ type: SELECT_TAB, tab });
 }
 
+export const SCORE_REQUEST: string = 'SCORE_REQUEST';
+export const SCORE_SUCCESS: string = 'SCORE_SUCCESS';
+export const SCORE_FAILURE: string = 'SCORE_FAILURE';
 export const SELECT_TAB: string = 'SELECT_TAB';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,24 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import {
-  SELECT_TAB
+  SELECT_TAB,
+  SCORE_REQUEST,
+  SCORE_SUCCESS,
+  SCORE_FAILURE
 } from '../actions';
+
+const score = (state = 0, action : { type: string }) => {
+  switch (action.type) {
+    case SCORE_REQUEST:
+      return state;
+    case SCORE_SUCCESS:
+      return action.response;
+    case SCORE_FAILURE:
+      return state;
+    default:
+      return state;
+  }
+}
 
 const tab = (state = 'home', action : { type: string, tab: string }) => {
   switch (action.type) {
@@ -16,6 +32,7 @@ const tab = (state = 'home', action : { type: string, tab: string }) => {
 
 const rootReducer = (history) => combineReducers({
   router: connectRouter(history),
+  score,
   tab
 });
 

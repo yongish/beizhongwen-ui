21825a1af7d92f29a0975f406c3e23125efba3e5
Zhi Yong Tan <yongish@gmail.com>
2019-11-17 16:42:09 -0800

    toggle button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,101 @@
-import React, { PureComponent } from 'react';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-class Home extends PureComponent {
-  render() {
-    return (
-      <div className='homeColumn flexDisplayColumn' style={{ border: '1px solid black' }}>
-        <h2 style={{ marginLeft: 20 }}>Top Words</h2>
-        <ButtonGroup
-          variant="contained"
-          color="primary"
-          aria-label="full-width contained primary button group"
-          style={{ marginLeft: 'auto', marginRight: 20 }}>
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
-      </div>
-    )
-  }
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
 }
 
-export default Home;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const [alignment, setAlignment] = React.useState('left');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Latest Words" {...a11yProps(0)} />
+          <Tab label="Most Views" {...a11yProps(1)} />
+          <Tab label="Highest Voted" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <div style={{ border: '1px solid black'}} className='flexDisplayColumn'>
+        <ToggleButtonGroup
+          value={alignment}
+          exclusive
+          onChange={handleAlignment}
+          aria-label="text alignment"
+          style={{ marginLeft: 'auto', marginRight: 0 }}
+        >
+          <ToggleButton value="left" aria-label="left aligned">
+            Week
+          </ToggleButton>
+          <ToggleButton value="center" aria-label="centered">
+            Month
+          </ToggleButton>
+          <ToggleButton value="right" aria-label="right aligned">
+            All time
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        </div>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Most Views
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Highest Voted
+      </TabPanel>
+    </div>
+  );
+}

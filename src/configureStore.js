8ac3f5f8fe444f70d65c536c40506dc425437db6
Zhi Yong Tan <yongish@gmail.com>
2019-12-25 00:06:35 -0800

    Remove persistedState code that wasn't in use.

--- src/configureStore.js
+++ src/configureStore.js
@@ -19,22 +19,8 @@ const composedEnhancers = composeWithDevTools(
 );
 
 export default function configureStore(preloadedState) {
-  const persistedState = localStorage.getItem("reduxState")
-    ? JSON.parse(localStorage.getItem("reduxState"))
-    : {};
-  const startState = (({login, tab, email, firstName, lastName}) => ({
-    login,
-    tab,
-    email,
-    firstName,
-    lastName
-  }))(persistedState);
-
   const store = createStore(
     createRootReducer(history), // root reducer with router state
-    // preloadedState,
-    // persistedState,
-    startState,
     composedEnhancers
   );
 

d57ffb4fbb5937961e569acaa71c7e04f7f72839
Zhi Yong Tan <yongish@gmail.com>
2019-12-27 11:18:26 -0800

    yarn remove redux-persist

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,8 +7,6 @@ import CardContent from "@material-ui/core/CardContent";
 import Collapse from "@material-ui/core/Collapse";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import IconButton from "@material-ui/core/IconButton";
 import Link from "@material-ui/core/Link";

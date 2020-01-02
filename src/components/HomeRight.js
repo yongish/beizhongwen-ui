dad8a970e390b6482a01d2818272e76aadcbb51c
Zhi Yong Tan <yongish@gmail.com>
2020-01-02 12:09:17 -0800

    Remove padding 20 from HomeRight.js.

--- src/components/HomeRight.js
+++ src/components/HomeRight.js
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function HomeRight() {
-  return (
-    <div style={{padding: 20}} className="homeRightColumn flexGrowOne"></div>
-  );
+  return <div className="homeRightColumn flexGrowOne"></div>;
 }
